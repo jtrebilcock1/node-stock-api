5d2cc7c540078d87ca1de67904b56e7e81c6fac7
joe trebilcock <jjoetrebil@gmail.com>
2023-07-19 11:42:57 -0400

    bootstrap and handlebar routing added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
@@ -7,14 +7,26 @@ const PORT = process.env.PORT || 5000;
 //Set handlebars
 
 app.engine('handlebars', exphbs.engine());
+app.set("views", "./views");
 app.set('view engine', 'handlebars');
+
+
+const otherstuff = "Content for a homepage";
 
 // Set handlenbars routes
 app.get('/', function (req, res) {
-res.render('home');
+    res.render('home', {
+        stuff: otherstuff
+    });
 });
+
+// Set handlenbars About routes
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
+
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log ('Server Listening on port ' + PORT));
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
