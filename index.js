707bfad8ae24806d4b989d05cf1d28eb8de8f638
joe trebilcock <jjoetrebil@gmail.com>
2023-07-19 16:02:50 -0400

    form middleware added ready for api

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
 //consider using got or something else instead of request
 const request = require('request');
+const bodyParser = require('body-parser')
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+
 
 
 //api
@@ -30,13 +40,27 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "Content for a homepage";
 
-// Set handlenbars routes
+// Set handlenbars index GET route
 app.get('/', function (req, res) {
     //api_call has a callback function
     api_call(function (doneAPI) {
         //body  of doneAPI. api_call will wait for this callback function to finish so the api doesnt return undefined
         res.render('home', {
             stock: doneAPI
+        });
+    });
+
+});
+
+// Set handlenbars index POST route
+app.post('/', function (req, res) {
+    //api_call has a callback function
+    api_call(function (doneAPI) {
+        //body  of doneAPI. api_call will wait for this callback function to finish so the api doesnt return undefined
+        posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: doneAPI,
+            posted_stuff: posted_stuff
         });
     });
 
